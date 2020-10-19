b7f53b9e833ec45c9a2362ecf67e1891b2a4c523
Aurelijus Lape <lape_aurelijus@yahoo.com>
2020-10-19 15:03:00 +0300

    Initialize repository

---
+++ lib/aurisimo_palindrome.rb
@@ -0,0 +1,6 @@
+require "aurisimo_palindrome/version"
+
+module AurisimoPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "aurisimo_palindrome"
+
+require "minitest/autorun"
