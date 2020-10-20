24e11eece01750185413c28e4ac8986f72811bab
Aurelijus Lape <lape_aurelijus@yahoo.com>
2020-10-20 10:44:11 +0300

    Finish working and refactoring palindeom method

--- lib/aurisimo_palindrome.rb
+++ lib/aurisimo_palindrome.rb
@@ -1,6 +1,16 @@
 require "aurisimo_palindrome/version"
 
-module AurisimoPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      gsub(/\W/, "").downcase
+    end
 end
+              
